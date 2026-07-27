76deae14f941c6694d3c3aad41a8be12b3f39bac
김세준 <chacott0518@gmail.com>
2026-07-27 21:45:41 +0900

    optimize long guide image

---
+++ resize-long.js
@@ -0,0 +1,8 @@
+﻿const sharp = require('sharp');
+
+sharp('public/images/임신중절수술/임신중절수술.webp')
+  .resize({ width: 760, withoutEnlargement: true })
+  .webp({ quality: 68 })
+  .toFile('public/images/임신중절수술/임신중절수술-optimized.webp')
+  .then(() => console.log('완료'))
+  .catch(err => console.error('에러:', err));
